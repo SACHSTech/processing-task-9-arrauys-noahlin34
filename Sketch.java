914b21f13cc9ef5326b9025898e4e6fccc18f89d
noahlin34 <nglin34@gmail.com>
2022-05-09 17:37:57 -0400

    asd

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,17 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+
+  float[] circleY = new float[15];
+  float[] circleX = new float[15];
+  boolean[] circleHideStatus = new boolean[15];
+  float[] squareX = {310, 340, 370};
+
+
+
+  int lives = 3;
+
+
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -17,19 +28,68 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
+
+    for(int i = 0; i < circleX.length; i++) {
+      circleX[i] = random(width);
+    }
+
+    for(int i = 0; i < circleHideStatus.length; i++) {
+      circleHideStatus[i] = false;
+    }
+
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    background(50);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  
+	  for(int i = 0; i < lives; i++) {
+      fill(255, 0, 0);
+      rect(squareX[i], 20, 20, 20);
+    }
+    
+
+    fill(255, 255, 255);
+    for (int i = 0; i < circleY.length; i++) {
+
+      ellipse(circleX[i], circleY[i], 25, 25);
+
+ 
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+
+      if(circleX[i] > width) {
+        circleX[i] = 0;
+      }
+
+      if(keyPressed){
+        if(keyCode == DOWN) {
+          circleY[i]++;
+        }
+        else if (keyCode == UP){
+          circleY[i]--;
+        }
+      }
+
+      circleY[i]  +=2;
+
+    }
+  
+
+    
+ 
+
+
   }
   
   // define other methods down here.
